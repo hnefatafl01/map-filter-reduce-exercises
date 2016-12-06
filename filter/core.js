859a2fa5b286404d9528f9328ab2da79420a87bb
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-12-05 19:14:44 -0700

    shift right started

--- filter/core.js
+++ filter/core.js
@@ -1,14 +1,37 @@
 function onlyEven (array) {
   // your code here
+  return array.filter(function(index) {
+    if (index % 2 === 0) {
+      return index;
+    }
+  })
 };
 
 function onlyOneWord (array) {
   // your code here
+  // return array.filter(function(index) {
+  //   var single;
+  //   for (var i = 0; i < index.length; i++) {
+  //     if (index[i] === " ") {
+  //       single = index;
+  //       return single;
+  //     } else {
+  //       return null;
+  //     }
+  //   }
+  //   if (single) {
+  //     return single;
+  //   }
+  // })
 };
 
 function positiveRowsOnly (array) {
-  // your code here
+  //your code here
+  //iterate through array
+  //filter each index array for positives
 };
+
+
 
 function allSameVowels (array) {
   // your code here
--- map/core.js
+++ map/core.js
@@ -1,14 +1,35 @@
 function multiplyBy10 (array) {
  // your code here
+  var times10 = array.map(function(number) {
+    return number * 10;
+  })
+  return times10;
 };
 
-function shiftRight (array) {
- // your code here
-};
+function shiftRight(array) {
+  return array.map(function(val,index) {
+    if (index === 0) {
+      return array[array.length-1];
+    }
+    else if (index === array.length-1) {
+      return array[0];
+    }
+    else {
+      return array[index]
+    }
+  });
+}
+
+shiftRight([{ name: '' }, 10, "left-side"]);
 
 function onlyVowels (array) {
  // your code here
+  // return array.map(function(index) {
+  // return   /[aeiou]/.text(chr.toLowerCase()
+  // })
 };
+// onlyVowels(['average', 'exceptional', 'amazing']);
+// let objs = [{name:''} , 10, "left-side"];
 
 function doubleMatrix (array) {
  // your code here
